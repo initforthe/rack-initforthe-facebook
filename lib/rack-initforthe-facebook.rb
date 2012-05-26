b25eff6f42e2ceb04e0beccf5f1b998357d87448
Rob Holland <rob@initforthe.com>
2012-05-26 16:36:22 +0100

    Add file to ease loading.

---
+++ lib/rack-initforthe-facebook.rb
@@ -0,0 +1,2 @@
+require 'rack/initforthe/facebook/signed_request'
+require 'rack/initforthe/facebook/method_fix'
--- lib/rack/initforthe/facebook/version.rb
+++ lib/rack/initforthe/facebook/version.rb
@@ -1,7 +1,7 @@
 module Rack
   module Initforthe
     module Facebook
-      VERSION = "0.0.1"
+      VERSION = "0.0.2"
     end
   end
 end
